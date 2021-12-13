bf1f029a83ee8f855b09bbd33ec21e57f2736b7c
Anghelina Semionov <anghelina.semionov@isa.utm.md>
2021-12-13 19:56:04 +0200

    delete bla

--- SyncNode/Services/SyncWorkJobService.cs
+++ SyncNode/Services/SyncWorkJobService.cs
@@ -60,7 +60,7 @@ namespace SyncNode.Services
                             var result = HttpClientUtility.SendJson(entity.JsonData, url, entity.SyncType);
                             if (!result.IsSuccessStatusCode)
                             {
-                                // lala bla bla
+                                // lala
                             }
                         }
                         catch (Exception e)
